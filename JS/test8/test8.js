30ff38c24747a6edd21653a6f830be4695a8a609
Airlo-eng <66566518+Airlo-eng@users.noreply.github.com>
2021-10-05 21:22:36 +0100

    test8.js
    Test 8 javascript added

---
+++ JS/test8/test8.js
@@ -0,0 +1,27 @@
+// array = a special variable,
+//         that can hold more than one value.
+
+//         Each "space" is known as an element
+
+//         You access elements in an array by referring
+//         to the index number ex.[0]
+
+var cars = ["Mustang", "Corvet", "Jaguar"];
+
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
+
+cars.push("Tesla"); // adds an element
+cars.pop(); // removes last element
+
+var numberOfCars = cars.length; // returns length of array
+
+// cars = cars.sort(); // sorts array alphabetically/numerically
+// cars = cars.reverse(); // reverse sort an array
+
+var lastCar = cars[cars.length - 1];
+
+console.log(cars);
+console.log(numberOfCars);
+console.log(lastCar);
