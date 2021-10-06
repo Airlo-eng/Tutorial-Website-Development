0f356ca7ad40badaba800c989272a3235631d3b6
Airlo-eng <66566518+Airlo-eng@users.noreply.github.com>
2021-10-06 20:47:07 +0100

    test15.js
    Test 15 javascript added

---
+++ JS/test14/test14.js
@@ -0,0 +1,18 @@
+// while looo = repeats some code, while the loop's condition remains true
+
+var creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE!");
+
+/*
+while (creditNum == "") {
+    var creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE!");
+}
+*/
+
+/*
+do{
+    var creditNum = window.prompt("YOUR COMPUTER HAS A VIRUS!! ENTER YOUR CREDIT CARD INFO... OR ELSE!");
+} while (creditNum == "");
+*/
+
+console.log("THANK YOU! You're computer no longer has a virus!");
+
