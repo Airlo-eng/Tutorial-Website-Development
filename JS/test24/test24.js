8603735f6534bf14d47d82d1c7ce0246d38350c8
Airlo-eng <66566518+Airlo-eng@users.noreply.github.com>
2021-10-08 21:49:26 +0100

    test24.js

---
+++ JS/test24/test24.js
@@ -0,0 +1,29 @@
+// hoisting = JS's default behavior of moving declarations,
+//            to the top of the current scope. A variable can be used
+//            before it has been declared. Beneficial to understand to help avoid bugs
+
+//            good practice to declare all variables
+//            at the top of every scope
+
+// ***********
+// Declaration
+// ***********
+
+/*
+myName = "Airlo";
+
+console.log(myName);
+
+var myName;
+const myName;
+*/
+
+// ***********
+// Initialize
+// ***********
+
+var firstName = "Airlo";
+
+console.log(firstName, lastName);
+
+var lastName = "-eng";
