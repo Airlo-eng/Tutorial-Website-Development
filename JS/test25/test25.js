b58fa2be4eb4bc093a2389e3d049f191c5728271
Airlo-eng <66566518+Airlo-eng@users.noreply.github.com>
2021-10-08 21:59:02 +0100

    test25.js

---
+++ JS/test25/test25.js
@@ -0,0 +1,19 @@
+// scope = where a variable is accessible
+
+// let = declare variables with a block scoped {}
+// var = declare variables with a function scoped ()
+
+var name = "Airlo-eng"; // can accidentally override important variables
+let name = "";
+
+/*
+function doSomething() {
+    for (var i = 1; i <= 3; i++) {
+        console.log(i);
+    }
+}
+
+doSomething();
+
+console.log(i);
+*/
